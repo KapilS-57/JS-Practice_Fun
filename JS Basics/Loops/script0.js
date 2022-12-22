5830ae3f7bf2741d27263666c842799847a38a3f
Kapil Sharma <capishr57@gmail.com>
2022-12-22 23:16:59 +0530

    Loops learning covered

---
+++ JS Basics/Loops/script0.js
@@ -0,0 +1,83 @@
+// ======================================
+// WITHOUT ANY LOOPS...LOTS OF REPETITION
+// ======================================
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// ======================================
+// OUR FIRST FOR LOOP
+// ======================================
+
+for (let num = 1; num <= 10; num += 1) {
+    console.log(num);
+}
+
+// ======================================
+// MORE EXAMPLES OF FOR LOOPS!
+// ======================================
+
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
+// for (let i = 10; i <= 1000; i *= 10) {
+//     console.log(i);
+// }
+
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
+// ================================
+// ================================
+// for (let i = 20; i >= 0; i++) {
+//     console.log(i)
+// }
+// ================================
+// ================================
+
+
+
+
+
+// //
+// // // ===================================
+// // NESTED FOR LOOP EXAMPLES
+// // ===================================
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`i is: ${i}`)
+//     for (let j = 1; j < 4; j++) {
+//         console.log(`     j is: ${j}`)
+//     }
+// }
+
+// // ===================================
+// // NESTED ARRAY ITERATION
+// // ===================================
+
+
+// const seatingChart = [
+//     ['Kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//     const row = seatingChart[i];
+//     console.log(`ROW #${i + 1}`)
+//     for (let j = 0; j < row.length; j++) {
+//         console.log(row[j])
+//     }
+// }
+
